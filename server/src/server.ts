2353ec4733a65765c653ff20a0fa9314d05e0d9e
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-01-17 12:08:07 -0300

    feat: adds listen habits at route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,25 @@
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 
 const app = fastify();
+const prisma = new PrismaClient();
+
+app.register(cors);
 
 /*
 HTTP Methods: Get, Post, Patch, Put and Delete
 */
+app.get("/", async () => {
+  const habits = await prisma.habit.findMany();
 
-app.get("/", () => {
-  return "Hello World";
+  return habits;
 });
 
-app.listen({
-  port: 3333,
-});
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server running!");
+  });
