1aec5309e1d19aca49ea953f44a0830c662511bb
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-01-18 15:55:37 -0300

    feat: creates SummaryTable component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 // import { Habits } from "./components/Habits";
 import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 import "./styles/global.css";
 
 export function App() {
@@ -7,6 +8,7 @@ export function App() {
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
+        <SummaryTable />
       </div>
     </div>
   );
---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,40 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
+import { HabitDay } from "./HabitDay";
+
+const WEEK_DAYS = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+const summaryDates = generateDatesFromYearBeginning();
+
+const minimumSummaryDatesSize = 18 * 7; // 18 weeks
+const amountDaysToFill = minimumSummaryDatesSize - summaryDates.length;
+
+export function SummaryTable() {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {WEEK_DAYS.map((weekDay, index) => (
+          <div
+            key={`${weekDay} - ${index}`}
+            className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+          >
+            {weekDay}
+          </div>
+        ))}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map((date) => (
+          <HabitDay key={date.toString()} />
+        ))}
+
+        {amountDaysToFill > 0 &&
+          Array.from({ length: amountDaysToFill }).map((_, index) => (
+            <div
+              key={index}
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
