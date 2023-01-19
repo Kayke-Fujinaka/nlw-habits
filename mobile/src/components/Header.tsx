ffa849b32af16c5ef6ecf6aa78d0814fb2e3e542
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-01-19 17:21:27 -0300

    feat: navegate to register screen

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,11 +1,14 @@
 import { Text, TouchableOpacity, View } from "react-native";
 
 import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 import colors from "tailwindcss/colors";
 import Logo from "../assets/logo.svg";
 
 export function Header() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
@@ -13,6 +16,7 @@ export function Header() {
       <TouchableOpacity
         activeOpacity={0.7}
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+        onPress={() => navigate("new")}
       >
         <Feather name="plus" color={colors.violet[500]} size={20} />
 
