f9da9a6cd43e4dfcc02092ef2b92c0c07e0340dc
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-01-19 11:39:09 -0300

    feat: creates routes to toggle habit

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -39,6 +39,7 @@ export function Home() {
           {amountOfDaysToFill > 0 &&
             Array.from({ length: amountOfDaysToFill }).map((_, index) => (
               <View
+                key={index}
                 className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
                 style={{ width: DAY_SIZE, height: DAY_SIZE }}
               />
