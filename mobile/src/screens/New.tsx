4d4954163b529662bedb9ac36eb48af1aea46cae
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-01-19 17:45:36 -0300

    feat: creates BackButton component

---
+++ mobile/src/screens/New.tsx
@@ -0,0 +1,12 @@
+import { ScrollView, View } from "react-native";
+import { BackButton } from "../components/BackButton";
+
+export function New() {
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <BackButton />
+      </ScrollView>
+    </View>
+  );
+}
