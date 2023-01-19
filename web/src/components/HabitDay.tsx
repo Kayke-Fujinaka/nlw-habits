402e81b62846f66363e2d3f3b36468b246b5be69
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-01-19 16:36:16 -0300

    feat: creates HabitDay component

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,9 +1,27 @@
+import * as Popover from "@radix-ui/react-popover";
+import { ProgressBar } from "./ProgressBar";
+
 // interface HabitDayProps {
 //   completed: number;
 // }
 
 export function HabitDay() {
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg cursor-pointer" />
+    <Popover.Root>
+      <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg cursor-pointer" />
+
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounder-2xl bg-zinc-900 flex flex-col">
+          <span className="font-semibold text-zinc-400">terça-feira</span>
+          <span className="mt-1 font-extrabold leading-tight text-3xl">
+            19/01
+          </span>
+
+          <ProgressBar progress={15} />
+
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 }
