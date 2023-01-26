cc292dbe5dcf9af379a96903e1c8dd23bd53089d
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-01-26 19:53:24 -0300

    feat: creates service worker

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,39 @@
 // import { Habits } from "./components/Habits";
 import { Header } from "./components/Header";
 import { SummaryTable } from "./components/SummaryTable";
+import { api } from "./lib/axios";
 import "./lib/dayjs";
 import "./styles/global.css";
+
+navigator.serviceWorker
+  .register("service-worker.js")
+  .then(async (serviceWorker) => {
+    let subscription = await serviceWorker.pushManager.getSubscription();
+
+    if (!subscription) {
+      const publicKeyResponse = await api.get("/push/public_key");
+
+      subscription = await serviceWorker.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: publicKeyResponse.data.publicKey,
+      });
+    }
+
+    await api.post("/push/register", {
+      subscription,
+    });
+
+    await api.post("/push/send", {
+      subscription,
+    });
+  });
 
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <button></button>
+
         <Header />
         <SummaryTable />
       </div>
